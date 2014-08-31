28d3ab3426c11a7a285075abab11591c542cbca0
chenxiinnj <1003681345@qq.com>
2014-08-31 16:35:42 +0800

    删除文件

--- countLine.py
+++
@@ -1,9 +0,0 @@
-def afileline(f_path):
-    #初始化行数
-    res = 0
-    f = open(f_path,'r')
-    for lines in f:
-        #便利行数，去掉空行（空行，split函数返回假）
-        if lines.split():
-            res += 1
-    return res
